2522ed71693fafb85c7269723ce5893ed21db6c2
Igor Kamyshev <igor@kamyshev.me>
2018-11-29 17:23:48 +0300

    Add simple implementation

--- lib/key-value-store.ts
+++ lib/key-value-store.ts
@@ -1,9 +1,21 @@
+interface Store {
+  [key: string]: any
+}
+
 export class KeyValueStore {
+  private store: Store = {}
+
   public init = () => null
 
-  public set = (key: string, value: any) => null
+  public set = (key: string, value: any): void => {
+    this.store[key] = value
+  }
 
-  public get = (key: string) => null
+  public get = (key: string): any => {
+    return this.store[key]
+  }
 
-  public delete = (key: string) => null
+  public delete = (key: string): void => {
+    this.store[key] = undefined
+  }
 }
